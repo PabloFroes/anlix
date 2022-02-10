525d330a5cfa5fa0ba9db6c9113ae78cbc5ccaf8
Pablo Froes <pablofroes@outlook.com>
2022-02-10 02:25:18 -0300

    Implemented converter file from dados/indice_pulmonar to csv

--- src/database/useCases/converterFile/ConverterFileUseCase.ts
+++ src/database/useCases/converterFile/ConverterFileUseCase.ts
@@ -1,16 +1,30 @@
 import fs from "fs"
 const indice_cardiacoPath = "./dados/indice_cardiaco";
+const indice_pulmonarPath = "./dados/indice_pulmonar";
 
 class ConverterFileUseCase {
 
-    execute():void{
+    execute() {
         fs.readdir(indice_cardiacoPath, (err,data) => {
             if(err) throw err
                 data.forEach((file)=>{
                     if(file.endsWith(".csv")){
-                        //console.log("Already converted to csv")
+                        console.log("Already converted to csv")
                     } else{
                         fs.rename(`${indice_cardiacoPath}/${file}`,`${indice_cardiacoPath}/${file}.csv`,(err)=>{
+                        if(err) throw err
+                        //console.log(`Converted ${file} to ${file}.csv`)
+                    })
+                }
+            })
+        })
+        fs.readdir(indice_pulmonarPath, (err,data) => {
+            if(err) throw err
+                data.forEach(async (file)=>{
+                    if(file.endsWith(".csv")){
+                        console.log("Already converted to csv")
+                    } else{
+                        fs.rename(`${indice_pulmonarPath}/${file}`,`${indice_pulmonarPath}/${file}.csv`,(err)=>{
                         if(err) throw err
                         //console.log(`Converted ${file} to ${file}.csv`)
                     })
